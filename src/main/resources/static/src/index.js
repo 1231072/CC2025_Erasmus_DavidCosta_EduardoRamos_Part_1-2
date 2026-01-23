c05cf40846cb0518ca686b75635f1cfb998e84d8
davidcosta <1231072@isep.ipp.pt>
2026-01-23 09:18:42 +0200

    feat: redirect to web url based on window.location.origin

--- src/main/resources/static/src/index.js
+++ src/main/resources/static/src/index.js
@@ -7,7 +7,8 @@ import { AuthProvider } from "react-oidc-context"; // OIDC da AWS
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_k4a9qUIbH",
   client_id: "2jumpgbb0cn80gqhfg2tth8f8",
-  redirect_uri: "http://localhost:3000",
+  redirect_uri: window.location.origin,
+  post_logout_redirect_uri: window.location.origin,
   response_type: "code",
   scope: "email openid profile",
   revokeTokenTypes: ["access_token", "refresh_token"],
